53a96ee9e77323cfc5c293ed1ab6701dabfb485b
Nata Chikhinashvili <natanatuka33@gmail.com>
2023-10-01 10:26:23 +0400

    feat: add to interests

--- index.py
+++ index.py
@@ -1,7 +1,7 @@
 from flask import jsonify, request, flash, Flask
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
-from usermodel import app, User, bcrypt, db
+from usermodel import app, User, bcrypt, db, Book
 
 
 @app.route('/')
@@ -37,7 +37,102 @@ def register():
         db.session.commit()
 
         return jsonify({"message": "Registration successful"}), 201
-
     
+@app.route('/add_book', methods=['POST'])
+def add_book():
+    if request.method == 'POST':
+        title = request.form.get('title')
+        author = request.form.get('author')
+        genre = request.form.get('genre')
+        user_id = request.form.get('user_id')
+
+        user = User.query.get(user_id)
+        if user is None:
+            return jsonify({"message": "User not found"}), 404
+
+        new_book = Book(title=title, author=author, genre=genre, user_id=user_id)
+
+        db.session.add(new_book)
+        db.session.commit()
+
+        return jsonify({"message": "Book added successfully"}), 201
+@app.route('/books', methods=['GET'])
+def books():
+    if request.method == 'GET':
+        books = Book.query.all()
+
+        book_list = []
+        for book in books:
+            book_data = {
+                'id': book.id,
+                'title': book.title,
+                'author': book.author,
+                'genre': book.genre,
+                'user_id': book.user_id
+            }
+            book_list.append(book_data)
+
+        return jsonify({'books': book_list})
+    
+@app.route('/change_ownership', methods=['POST'])
+def change_ownership():
+    if request.method == 'POST':
+        # Get the book ID and the new owner's email from the request
+        book_id = request.form.get('book_id')
+        new_owner_email = request.form.get('new_owner_email')
+
+        # Check if the book with the specified book_id exists
+        book = Book.query.get(book_id)
+        if book is None:
+            return jsonify({"message": "Book not found"}), 404
+
+        # Check if the new owner with the specified email exists
+        new_owner = User.query.filter_by(email=new_owner_email).first()
+        if new_owner is None:
+            return jsonify({"message": "New owner not found"}), 404
+
+        # Check if the new owner has an interest in the book
+        if book.title not in new_owner.interests:
+            return jsonify({"message": "New owner is not interested in this book"}), 400
+
+        # Update the book's owner to the new owner
+        book.user_id = new_owner.id
+        db.session.commit()
+        return jsonify({"message": "Ownership changed successfully"}), 200
+    
+@app.route('/add_interest', methods=['POST'])
+def add_interest():
+    if request.method == 'POST':
+        book_id = request.form.get('book_id')
+        recepient = request.form.get('recepient')
+
+        book = Book.query.get(book_id)
+        if book is None:
+            return jsonify({"message": "Book not found"}), 404
+
+        if book.title not in recepient.interests:
+            recepient.interests.append(book.title)
+
+        book.user_id = recepient.id
+        db.session.commit()
+        return jsonify({"message": "Ownership changed successfully"}), 200
+
+@app.route('/request_interest', methods=['POST'])
+def request_interest():
+    if request.method == 'POST':
+        client_user_id = request.form.get('client_user_id')
+        book_title = request.form.get('book_title')
+
+        client_user = User.query.get(client_user_id)
+        if client_user is None:
+            return jsonify({"message": "Client user not found"}), 404
+        
+        url = 'http://localhost:5000/add_interest'
+        book = Book.query.filter_by(title=book_title).first()
+        recepient = Book.query.filter_by(id=client_user_id).first()
+        request.post(url, data={"book_id": book.id, "recepient": recepient})
+        
+        return jsonify({"message": "Request sent to owner"}), 200
+
 if __name__ == '__main__':
     app.run(debug=True)
